1c8df14d869173eca9b0e97413febb7c35e65522
git-zahidul-islam <freelancerzahidf101@gmail.com>
2024-03-16 01:00:37 +0600

    stat the assignment

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+    <div className='w-10/12 mx-auto'>
+      <Header></Header>
+    </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+// import PropTypes from 'prop-types';
+const Header = () => {
+    return (
+        <div>
+            <h1>hello bro</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, possimus.</p>
+        </div>
+    );
+};
+
+Header.propTypes = {
+    
+};
+
+export default Header;
